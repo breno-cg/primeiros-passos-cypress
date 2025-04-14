bd8bbaf2a6035959dbf0fbd2a6103d7bba05118f
Breno <brenocorrea.gomes@gmail.com>
2025-04-14 18:26:19 -0300

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,10 +8,17 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    employeeFirstNameField: "[name='firstName']",
+    employeeLastNameField: "[name='lastName']",
+    employeeGenericField: ".oxd-input--active",
+    dateCloseButton: ".--close",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dataSaveButton: "[type='submit']"
   }
 
-  it('Login - Success', () => {
+  it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     //cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
     //cy.get("[name='username']").type('Admin')
@@ -23,6 +30,18 @@ describe('Orange HRM Tests', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     //cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.employeeFirstNameField).clear().type(userData.employeeData.firstname)
+    cy.get(selectorsList.employeeLastNameField).clear().type(userData.employeeData.lastname)
+    cy.get(selectorsList.employeeGenericField).eq(3).clear().type(userData.employeeData.employeeid)
+    cy.get(selectorsList.employeeGenericField).eq(4).clear().type(userData.employeeData.otherid)
+    cy.get(selectorsList.employeeGenericField).eq(5).clear().type(userData.employeeData.driverslicensenumber)
+    cy.get(selectorsList.dateField).eq(0).clear().type(userData.employeeData.expirydate)
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateField).eq(1).clear().type(userData.employeeData.birthdate)
+    cy.get(selectorsList.dataSaveButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
   })
   it('Login - Fail', () => {
     cy.visit('/auth/login')
